575abc83bcdf53b5036943aa62f14c2c68cf9137
saharana2 <anjalisaharan1995@gmail.com>
2022-09-15 14:53:42 -0600

    phase-1

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  url: BASE_URL,
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "62291f0af0msh4a33e760d3550b0p152e6cjsnef687abbb2b1",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
